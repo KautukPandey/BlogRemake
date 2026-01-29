234197eff147e846dcac1a9f367b60afe908b846
Kautuk <kautukpandey2004@gmail.com>
2026-01-29 23:24:38 +0530

    Implement user authentication with JWT and cookie management

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
+const cookieParser = require('cookie-parser')
 const app = express()
-
+const authRoutes = require('./routes/authRoutes')
 // Parses incoming JSON requests and makes data available on req.body
 app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/auth',authRoutes)
 
 app.get('/health',(req,res)=>{
     res.send('Health route')
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,39 +2,85 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
 
   if (!name || !email || !password) {
-    return res.status(400).json({
-      message: "All fields are required",
-    });
+    return res.status(400).json({ message: "All fields are required" })
   }
 
-  const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email })
   if (userExists) {
-    return res.status(400).json({
-      message: "User already exists",
-    });
+    return res.status(400).json({ message: "User already exists" })
   }
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await User.create({ name, email, password })
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  )
+
+  
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000
+  })
+
   return res.status(201).json({
     message: "User registered successfully",
-    token,
     user: {
       id: user._id,
       name: user.name,
-      email: user.email,
-    },
-  });
-};
+      email: user.email
+    }
+  })
+}
 
-module.exports = { register };
+
+const login = async(req,res)=>{
+  const {email,password} = req.body
+  if(!email || !password){
+    return res.status(400).json({
+      message: "All fields are required",
+    }); 
+  }
+  const user = await User.findOne({email}).select('+password')
+  if(!user){
+    return res.status(400).json({ message: "User does not exists" })
+  }
+
+
+  const isMatch = await user.comparePassword(password)
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" })
+  }
+
+  const token = jwt.sign(
+    {id: user._id},
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '1h'
+    }
+  )
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000
+  })
+
+  return res.status(200).json({
+  message: 'Login successful',
+  user: {
+    id: user._id,
+    name: user.name,
+    email: user.email
+    }
+  })  
+}
+
+
+module.exports = { register, login };
